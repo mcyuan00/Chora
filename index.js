f8be7a46cb688784c9f67e0d039a21e2b863deaa
Amber Guo <ambrguo@gmail.com>
2016-04-10 16:11:40 -0400

    changing the side bar to include tabbing functionality

--- index.js
+++ index.js
@@ -2,3 +2,41 @@ $("#menu-toggle").click(function(e) {
 	e.preventDefault();
 	$("#wrapper").toggleClass("toggled");
 });
+
+var onClick = function () {
+  var sidebar = document.querySelector('.sidebar');
+  var body = document.querySelector('.body');
+  var angle = document.querySelector('.angle');
+  var classNames = document.querySelector('.sidebar').className;
+  if(classNames.indexOf('maximized') > -1) {
+    sidebar.className = 'sidebar';
+    body.className = 'body minimized';
+    angle.className = 'angle icon-double-angle-right';
+    $('.tab-content').hide();
+  } else {
+    sidebar.className = 'sidebar maximized';
+    body.className = 'body';
+    angle.className = 'angle icon-double-angle-left';
+    $('.tab-content').show();
+  }
+  return false;
+};
+
+
+var tabsFn = (function() {
+  
+  function init() {
+    setHeight();
+  }
+  
+  function setHeight() {
+    var $tabPane = $('.tab-pane'),
+        tabsHeight = $('.sidebar').height();
+    
+    $tabPane.css({
+      height: tabsHeight,
+    });
+  }
+    
+  $(init);
+})();
